0d6fc42840ca2b5d1bb74c1cae216a13d455ffe4
Lepikhin Egor <lepikhin.egor@yandex.ru>
2020-05-07 13:34:37 +0300

    add default value for country field in cardealer editor

--- modules/web/src/com/egorl/car_service/web/screens/cardealer/CarDealerEdit.java
+++ modules/web/src/com/egorl/car_service/web/screens/cardealer/CarDealerEdit.java
@@ -1,11 +1,32 @@
 package com.egorl.car_service.web.screens.cardealer;
 
+import com.egorl.car_service.entity.Country;
+import com.egorl.car_service.service.CountryService;
+import com.haulmont.cuba.gui.components.LookupPickerField;
 import com.haulmont.cuba.gui.screen.*;
 import com.egorl.car_service.entity.CarDealer;
+
+import javax.inject.Inject;
 
 @UiController("carservice_CarDealer.edit")
 @UiDescriptor("car-dealer-edit.xml")
 @EditedEntityContainer("carDealerDc")
 @LoadDataBeforeShow
 public class CarDealerEdit extends StandardEditor<CarDealer> {
+
+    @Inject
+    LookupPickerField<Country> countryField;
+
+    private CountryService countryService;
+
+    @Inject
+    public void setCountryService(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        if( getEditedEntity().getCountry() == null)
+            countryField.setValue(countryService.getDefaultCountry());
+    }
 }
